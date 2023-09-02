995e1d589273ac03248408769312ca44fc7489b0
Rodrigo da Hora Oliveira <rodrigoliveira328@hotmail.com>
2023-09-01 23:01:44 -0300

    Filtros dinamicos com Where

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -16,9 +16,40 @@ class ModeloController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Response()->json($this->modelo->with('marca')->get(), 200);
+        $modelos = array();
+
+        if ($request->has('atributos_marca')) {
+            $atributos_marca = $request->atributos_marca;
+            $modelos = $this->modelo->with('marca:id,'.$atributos_marca);
+        }else {
+            $modelos = $this->modelo->with('marca');
+        }
+
+        if ($request->has('filtro')) {
+            $filtros = explode(';', $request->filtro);
+            foreach($filtros as $condicao) {
+
+                $c = explode(':', $condicao);
+                
+                $modelos = $modelos->where($c[0], $c[1], $c[2]);
+
+            }
+            
+        }
+        
+
+        if ($request->has('atributos')) {
+            $atributos = $request->atributos;
+            
+            $modelos = $modelos->selectRaw($atributos)->get();
+            
+        }else {
+            $modelos = $modelos->get();
+        }
+
+        return Response()->json($modelos, 200);
     }
 
     /**
