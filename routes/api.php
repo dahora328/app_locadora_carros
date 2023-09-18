386c4dd4d3dc97ad65d23d70994572b666dbe16c
Dahora <rodrigoliveira328@hotmail.com>
2023-09-18 11:23:43 -0300

    Middleware nas rotas

--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('cliente', ClienteController::class);
-Route::apiResource('carro', CarroController::class);
-Route::apiResource('locacao', LocacaoController::class);
-Route::apiResource('marca', MarcaController::class);
-Route::apiResource('modelo', ModeloController::class);
+Route::apiResource('cliente', ClienteController::class)->middleware('jwt.auth');
+Route::apiResource('carro', CarroController::class)->middleware('jwt.auth');
+Route::apiResource('locacao', LocacaoController::class)->middleware('jwt.auth');
+Route::apiResource('marca', MarcaController::class)->middleware('jwt.auth');
+Route::apiResource('modelo', ModeloController::class)->middleware('jwt.auth');
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout',[AuthController::class,'logout']);
