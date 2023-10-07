f7b27fbd3e073e593e7d46e7f67ca241bf2e566c
Rodrigo da Hora Oliveira <rodrigoliveira328@hotmail.com>
2023-10-07 18:37:09 -0300

    Tirando dependencias do componente table

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -3,23 +3,35 @@
     <table class="table table-hover">
       <thead>
         <tr>
-          <th scope="col" v-for="t, key in titulos" :key="key">{{ t }}</th>
+          <th scope="col" v-for="t, key in titulos" :key="key" class="text-uppercase">{{ t }}</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="m in dados" :key="m.id">
+        <tr v-for="obj in dados" :key="obj.id">
+          <td v-for="valor, chave in obj" :key="chave">
+            <p v-if="titulos.includes(chave)">
+              <span v-if="chave == 'imagem'">
+                <img :src="'/storage/'+ valor" width="30" height="30">
+              </span>
+              <span v-else>
+                {{ valor }}
+              </span>
+            </p>
+          </td>
+
+          <!--
           <th scope="row">{{ m.id }}</th>
           <td>{{ m.nome }}</td>
-          <td> <img :src="'/storage/'+m.imagem" width="30" height="30"></td>
+          <td> <img :src="'/storage/'+m.imagem" width="30" height="30"></td>-->
         </tr>
       </tbody>
     </table>
-    
+
   </div>
 </template>
 
 <script>
 export default {
-  props: ['dados','titulos']
+  props: ['dados', 'titulos']
 };
 </script>
