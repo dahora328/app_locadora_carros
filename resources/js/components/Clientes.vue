814b41bf3522b450d81812af95bdf02b158da3eb
dahora <rodrigoliveira328@hotmail.com>
2023-10-31 17:24:36 -0300

    Carregar dados de clientes na tablea incompleto

--- resources/js/components/Clientes.vue
+++ resources/js/components/Clientes.vue
@@ -44,10 +44,10 @@
                     <div class="card mb-3">
                         <card titulo="Relação de Clientes">
                             <template v-slot:conteudo>
-                                <table-component :dados="marcas.data"
-                                :visualizar="{visivel: true, dataToggle: 'modal', dataTarget:'#modalMarcaVisualizar'}"
+                                <table-component :dados="clientes.data"
+                                :visualizar="{visivel: true, dataToggle: 'modal', dataTarget:'#modalClienteVisualizar'}"
                                 :atualizar="true"
-                                :remover="{visivel: true, dataToggle: 'modal', dataTarget:'#modalMarcaRemover'}"
+                                :remover="{visivel: true, dataToggle: 'modal', dataTarget:'#modalClienteRemover'}"
                                 :titulos="{
                                 id: { titulo: 'ID', tipo: 'texto' },
                                 nome: { titulo: 'Nome', tipo: 'texto' },
@@ -57,13 +57,15 @@
                             <template v-slot:rodape>
                                 <div class="col-10">
                                     <paginate-component>
-                                        <!--<li><a class="page-link"></a></li>-->
+                                        <li v-for="l, key in this.clientes.links" :key="key"
+                                            :class="l.active ? 'page-item active' : 'page-item'" @click="paginacao(l)"><a class="page-link"
+                                            v-html="l.label"></a></li>
                                     </paginate-component>
                                 </div>
                                 <div class="justify-content-md-end">
-                                    <!--<button type="submit" class="btn btn-primary btn-sm me-md-2">
+                                    <button type="submit" class="btn btn-primary btn-sm me-md-2">
                                         Adicionar
-                                    </button>-->
+                                    </button>
                                 </div>
                             </template>
                         </card>
@@ -78,18 +80,42 @@
     import axios from 'axios'
     import Alert from './Alert.vue'
     export default{
+        components: { Alert },
         data(){
             return {
                 urlBase: 'http://localhost/api/v1/cliente',
                 urlPaginacao: '',
                 urlFiltro: '',
                 nomeCliente: '',
-                marcas: { data: [] },
+                clientes: { data: [] },
                 busca: { id: '', nome: '' }
             }
         },
-        mounted() {
+        methods: {
+            paginacao(l) {
+                if (l.url) {
+                    //this.urlBase = l.url //ajustando a url de consulta com o parâmetro de página
+                    this.urlPaginacao = l.url.split('?')[1]
+                    //this.carregarLista() //requisitando novamente os dados para nossa API
+                }
+            },
+            carregarLista() {
 
+                let url = this.urlBase + '?' + this.urlPaginacao + this.urlFiltro
+
+                axios.get(url)
+                .then(response => {
+                    console.log(response.data)
+                    this.clientes = response.data
+                    //console.log(this.clientes)
+                })
+                .catch(erros => {
+                    console.log(erros)
+                })
+            },
+        },
+        mounted() {
+            this.carregarLista()
         }
     }
 </script>
